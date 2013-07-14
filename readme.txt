22a95d4d2c89bce32ad14c996b76b590969bc413
Shantanu Goel <shantanu@shantanugoel.com>
2013-07-14 18:46:08 +0530

    Add timestamps

--- readme.txt
+++ readme.txt
@@ -1,2 +1,26 @@
 A simply python script to send messages to Samsung TVs that can be displayed onscreen.
+Developed by Shantanu Goel http://tech.shantanugoel.com/
 Currently tested with Samsung 2010 C series TVs. Should work with most others as well that support this service.
+Run "python samsung-msgbox.py -h" for usage instructions
+
+usage: samsung-msgbox.py -i IP -m MSG [-p PORT] [-t TIME] [-r RECEIVER]
+                         [-x RECEIVER_NO] [-s SENDER] [-y SENDER_NO] [-h]
+
+Send an arbitrary text message to Samsung TVs which is displayed onscreen.
+Developed by Shantanu Goel (http://tech.shantanugoel.com/) version 1.0
+
+Arguments:
+  -i IP, --ip IP        Required. IP Address of the TV
+  -m MSG, --msg MSG     Required. Message body text to be sent to TV
+  -p PORT, --port PORT  Optional. Port on which message should be sent
+  -t TIME, --time TIME  Optional. Receive date and time in epoch/unix format
+  -r RECEIVER, --receiver RECEIVER
+                        Optional. Receiver Name
+  -x RECEIVER_NO, --receiverno RECEIVER_NO
+                        Optional. Receiver Number
+  -s SENDER, --sender SENDER
+                        Optional. Sender Name
+  -y SENDER_NO, --senderno SENDER_NO
+                        Optional. Sender Number
+  -h, --help
+
--- samsung-msgbox.py
+++ samsung-msgbox.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
+# Developed by Shantanu Goel. http://tech.shantanugoel.com/
 
 import socket
 import argparse
+import time
 
 #Defaults
 port        = 52235
@@ -9,9 +11,10 @@ receiver_no = "1234567890"
 sender_no   = "1234567890"
 receiver    = "Receiver"
 sender      = "Sender"
+epochtime   = time.mktime(time.localtime())
 
 def buildMessage(args):
-  body = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">" + "<s:Body>" + "      <u:AddMessage xmlns:u=\"urn:samsung.com:service:MessageBoxService:1\">" + "         <MessageType>text/xml</MessageType>" + "         <MessageID>MessageId</MessageID>" + "<Message>" + "&lt;Category&gt;SMS&lt;/Category&gt;" + "&lt;DisplayType&gt;Maximum&lt;/DisplayType&gt;" + "&lt;ReceiveTime&gt;" + "&lt;Date&gt;2010-05-04&lt;/Date&gt;" + "&lt;Time&gt;01:02:03&lt;/Time&gt;" + "&lt;/ReceiveTime&gt;" + "&lt;Receiver&gt;" + "&lt;Number&gt;" + args.receiver_no + "&lt;/Number&gt;" + "&lt;Name&gt;" + args.receiver + "&lt;/Name&gt;" + "&lt;/Receiver&gt;" + "&lt;Sender&gt;" + "&lt;Number&gt;" + args.sender_no + "&lt;/Number&gt;" + "&lt;Name&gt;" + args.sender + "&lt;/Name&gt;" + "&lt;/Sender&gt;" + "&lt;Body&gt;" + args.msg + "&lt;/Body&gt;" + "</Message>" + "      </u:AddMessage>" + "   </s:Body>" + "</s:Envelope>";
+  body = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">" + "<s:Body>" + "      <u:AddMessage xmlns:u=\"urn:samsung.com:service:MessageBoxService:1\">" + "         <MessageType>text/xml</MessageType>" + "         <MessageID>MessageId</MessageID>" + "<Message>" + "&lt;Category&gt;SMS&lt;/Category&gt;" + "&lt;DisplayType&gt;Maximum&lt;/DisplayType&gt;" + "&lt;ReceiveTime&gt;" + "&lt;Date&gt;" + time.strftime('%Y-%m-%d', time.localtime(args.time)) + "&lt;/Date&gt;" + "&lt;Time&gt;" + time.strftime('%H:%M:%S', time.localtime(args.time)) + "&lt;/Time&gt;" + "&lt;/ReceiveTime&gt;" + "&lt;Receiver&gt;" + "&lt;Number&gt;" + args.receiver_no + "&lt;/Number&gt;" + "&lt;Name&gt;" + args.receiver + "&lt;/Name&gt;" + "&lt;/Receiver&gt;" + "&lt;Sender&gt;" + "&lt;Number&gt;" + args.sender_no + "&lt;/Number&gt;" + "&lt;Name&gt;" + args.sender + "&lt;/Name&gt;" + "&lt;/Sender&gt;" + "&lt;Body&gt;" + args.msg + "&lt;/Body&gt;" + "</Message>" + "      </u:AddMessage>" + "   </s:Body>" + "</s:Envelope>";
 
   host = socket.gethostname()
   length = len(body)
@@ -34,12 +37,12 @@ def sendMessage(args, message):
   s.close()
 
 def main():
-  parser = argparse.ArgumentParser(description='Send Messages to Samsung TVs', add_help = False)
+  parser = argparse.ArgumentParser(description='Send an arbitrary text message to Samsung TVs which is displayed onscreen. Developed by Shantanu Goel (http://tech.shantanugoel.com/) version 1.0', add_help = False)
   flags = parser.add_argument_group('Arguments')
   flags.add_argument('-i', '--ip', dest = 'ip', default = None, help = 'Required. IP Address of the TV', required = True)
   flags.add_argument('-m', '--msg', dest = 'msg', default = None, help = 'Required. Message body text to be sent to TV', required = True)
-  flags.add_argument('-p', '--port', dest = 'port', default = port, type=int, help = 'Optional. Port on which message should be sent')
-  flags.add_argument('-t', '--time', dest = 'time', default = None, help = 'Optional. Receive date and time in unix format')
+  flags.add_argument('-p', '--port', dest = 'port', default = port, type = int, help = 'Optional. Port on which message should be sent')
+  flags.add_argument('-t', '--time', dest = 'time', default = epochtime, type = float, help = 'Optional. Receive date and time in epoch/unix format')
   flags.add_argument('-r', '--receiver', dest = 'receiver', default = receiver, help = 'Optional. Receiver Name')
   flags.add_argument('-x', '--receiverno', dest = 'receiver_no', default = receiver_no, help = 'Optional. Receiver Number')
   flags.add_argument('-s', '--sender', dest = 'sender', default = sender, help = 'Optional. Sender Name')
